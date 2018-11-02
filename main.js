6795877bcc083b6bf57fd3776fbfb698a8ad4210
Guan Hui Ye <oscar870524@gmail.com>
2018-11-02 09:50:21 +0800

    init

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    var list = [];
+
+    var app = new Vue({
+        el: "#app",
+        data: {
+            list: list
+        },
+        methods: {
+            remove: function(id) {
+                this.list.splice(id, 1);
+            }
+        }
+    })
+
+    var btnswitch = false;
+    var lotter
+    $("#btn_Start").click(function() {
+        if ($("#min").val() === "" || $("#max").val() === "" || parseInt($('#min').val()).toString() === "NaN" || parseInt($('#max').val()).toString() === "NaN") {
+            $(".meesage-box").css("display", "block");
+            $("#meesage").text("數字沒有打歐!");
+            $("#min").addClass("shadow");
+            $("#max").addClass("shadow");
+        } else {
+            $(".meesage-box").css("display", "none");
+            $("#meesage").text("");
+            $("#min").removeClass("shadow");
+            $("#max").removeClass("shadow");
+            if (!btnswitch) {
+                lotter = setInterval(function() {
+                    var numans = getRandom(parseInt($('#min').val()), parseInt($('#max').val()));
+                    $("#num").text(numans);
+                }, 10);
+                btnswitch = true;
+                $("#btn_Start").text("就決定是你了!");
+            } else {
+                clearInterval(lotter);
+                list.push($("#num").text());
+
+                btnswitch = false;
+                $("#btn_Start").text("開抽");
+            }
+        }
+
+
+    });
+
+});
