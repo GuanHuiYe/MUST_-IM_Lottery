7a2f6fb9ac4f4377d67f53dcebba039c0a682d3d
Guan Hui Ye <oscar870524@gmail.com>
2018-11-03 20:24:27 +0800

    修改過多資料çç有問題

--- main.js
+++ main.js
@@ -1,53 +1,55 @@
 $(document).ready(function() {
+  $(".main").css("top", $(window).height() / 2.0 - $(".main").height() / 2.0);
 
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  //   vue執行
+  var list = [];
+  var app = new Vue({
+    el: "#app",
+    data: {
+      list: list
+    },
+    methods: {
+      remove: function(id) {
+        this.list.splice(id, 1);
+      }
     }
+  });
 
-    var list = [];
+  //主要code
+  var btnswitch = false;
+  var lotter;
+  $("#btn_Start").click(function() {
+    if ($("#min").val() === "" || $("#max").val() === "") {
+      $(".meesage-box").css("display", "block");
+      $("#meesage").text("*數字沒有打歐!");
+      $("#min").addClass("shadow");
+      $("#max").addClass("shadow");
+    } else {
+      $(".meesage-box").css("display", "none");
+      $("#meesage").text("");
+      $("#min").removeClass("shadow");
+      $("#max").removeClass("shadow");
+      if (!btnswitch) {
+        lotter = setInterval(function() {
+          var numans = getRandom(
+            parseInt($("#min").val()),
+            parseInt($("#max").val())
+          );
+          $("#num").text(numans);
+        }, 10);
+        btnswitch = true;
+        $("#btn_Start").text("就決定是你了!");
+      } else {
+        clearInterval(lotter);
+        list.push($("#num").text());
 
-    var app = new Vue({
-        el: "#app",
-        data: {
-            list: list
-        },
-        methods: {
-            remove: function(id) {
-                this.list.splice(id, 1);
-            }
-        }
-    })
-
-    var btnswitch = false;
-    var lotter
-    $("#btn_Start").click(function() {
-        if ($("#min").val() === "" || $("#max").val() === "" ) {
-            $(".meesage-box").css("display", "block");
-            $("#meesage").text("數字沒有打歐!");
-            $("#min").addClass("shadow");
-            $("#max").addClass("shadow");
-        } else {
-            $(".meesage-box").css("display", "none");
-            $("#meesage").text("");
-            $("#min").removeClass("shadow");
-            $("#max").removeClass("shadow");
-            if (!btnswitch) {
-                lotter = setInterval(function() {
-                    var numans = getRandom(parseInt($('#min').val()), parseInt($('#max').val()));
-                    $("#num").text(numans);
-                }, 10);
-                btnswitch = true;
-                $("#btn_Start").text("就決定是你了!");
-            } else {
-                clearInterval(lotter);
-                list.push($("#num").text());
-
-                btnswitch = false;
-                $("#btn_Start").text("開抽");
-            }
-        }
-
-
-    });
-
+        btnswitch = false;
+        $("#btn_Start").text("開抽");
+      }
+    }
+  });
 });
